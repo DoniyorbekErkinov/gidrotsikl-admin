f93f3decb581857581e4ef4ea6d86e691091180a
doniyor <erkinovdoniyor03@gmail.com>
2022-12-15 00:11:52 +0500

    tables

--- src/Pages/Departments/index.js
+++ src/Pages/Departments/index.js
@@ -1,17 +1,60 @@
-import {useTranslation} from "react-i18next";
-import CardComponent from "../../Components/Card.component";
+// import {useTranslation} from "react-i18next";
+// import CardComponent from "../../Components/Card.component";
+import { useMemo } from "react";
+import { useTable } from "react-table";
 import ContentComponent from "../../Components/Content.component";
-function Department(params) {
-    
-    const {t} = useTranslation()
+import {COLUMNS} from './Columns';
+import MOCK_DATA from './MOCK_DATA.json';
+import './table.css'
+function Department() {
+    const columns = useMemo(() => COLUMNS,[])    
+    const data = useMemo(() => MOCK_DATA,[])    
+    const tableInstance = useTable({
+        columns,
+        data
+    });
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = tableInstance;
     return (
         <div>
             <ContentComponent>
-                <CardComponent>
-                    <div>
-                        <h1>123 54 79 98</h1>
-                    </div>
-                </CardComponent>
+                <table {...getTableProps}>
+                    <thead>
+                        {
+                            headerGroups.map((headerGroup) => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {
+                                        headerGroup.headers.map((header) => (
+                                            <th {...header.getHeaderProps()}>{header.render('Header')}</th>
+                                        ))
+                                    }
+                                </tr>
+                            ))
+                        }                        
+                    </thead>
+                    <tbody {...getTableBodyProps}>
+                        {
+                            rows.map((row) => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {
+                                            row.cells.map((cell) => {
+                                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                            })
+                                        }                                        
+                                    </tr>
+                                )}
+                            )
+                            
+                        }
+                    </tbody>
+                </table>
             </ContentComponent>
         </div>
     )
